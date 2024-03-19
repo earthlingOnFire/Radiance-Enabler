0624098009e5cbb87eed25a714ef5f0df84bfa7e
earthlingOnFire <78956999+earthlingOnFire@users.noreply.github.com>
2024-03-19 22:37:36 +0000

    First commit

---
+++ Plugin.cs
@@ -0,0 +1,62 @@
+﻿using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+using Object = UnityEngine.Object;
+using Configgy;
+
+namespace RadianceEnabler;
+
+[BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, PLUGIN_VERSION)]
+[BepInDependency("Hydraxous.ULTRAKILL.Configgy", BepInDependency.DependencyFlags.HardDependency)]
+public class Plugin : BaseUnityPlugin {	
+  public const string PLUGIN_GUID = "com.earthlingOnFire.RadianceEnabler";
+  public const string PLUGIN_NAME = "Radiance Enabler";
+  public const string PLUGIN_VERSION = "1.0.0";
+
+  private ConfigBuilder config;
+
+  private void Awake() {
+    config = new ConfigBuilder(PLUGIN_GUID, PLUGIN_NAME);
+    config.BuildAll();
+  }
+
+  private void Start() {
+    new Harmony(PLUGIN_GUID).PatchAll();
+  }
+}
+
+public class RadianceConfig : MonoBehaviour {
+  [Configgable("", "Radiance Tier")]
+  public static float radianceTier = 1f;
+
+  [Configgable("", "Toggle Radiance")]
+  public static ConfigToggle radianceToggle = new ConfigToggle(false);
+}
+
+[HarmonyPatch]
+public static class Patches {
+  [HarmonyPostfix]
+  [HarmonyPatch(typeof(OptionsManager), "Update")]
+  private static void OptionsManager_Update_Postfix() {
+    SetRadiance(RadianceConfig.radianceToggle.Value, RadianceConfig.radianceTier);
+  }
+
+  private static void SetRadiance(bool radiance, float tier) {
+    if (!SceneHelper.CurrentScene.Contains("Level") ||
+       (OptionsManager.forceRadiance == radiance && OptionsManager.radianceTier == tier)) {
+      return;
+    }
+
+    if (radiance) {
+      MonoSingleton<AssistController>.Instance.cheatsEnabled = true;
+    }
+
+    OptionsManager.forceRadiance = radiance;
+    OptionsManager.radianceTier = tier;
+
+    foreach(EnemyIdentifier enemy in Object.FindObjectsOfType<EnemyIdentifier>()) {
+      enemy.UpdateBuffs();
+    }
+  }
+}
+
---
+++ package/README.md
@@ -0,0 +1,9 @@
+WHAT DOES THIS MOD DO?
+- This mod allows you to enable and disable full radiance through Configgy.
+- Although you don't need to enable cheats beforehand, they will be enabled the moment you turn radiance on, even if the Cheats Menu is hidden.
+
+HOW DO I INSTALL THIS MOD MANUALLY?
+- Install BepInEx and Configgy. Once they are installed, all you need to do is drag RadianceEnabler.dll to BepInEx/plugins inside your game files.
+
+> [!NOTE]
+> Enabling radiance with "buffs forceradiance on" will not work when this mod is installed.
